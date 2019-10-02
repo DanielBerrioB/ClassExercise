a6df6a1247252d44bf893b2edd6c30e624218848
DanielBerrioB <daniberrio10@hotmail.com>
2019-10-01 19:55:39 -0500

    Nuevo release

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
 });
 
-router.get("/hola", function(req, res, next) {
+router.get("/hola", (req, res, next) => {
   res.render("index", { title: "New express" });
 });
 
+router.get("/hola", (req, res, next) => {
+  res.render("index", { title: "New express" });
+});
+router.get("/hola", (req, res, next) => {
+  res.render("index", { title: "New express" });
+});
 module.exports = router;
